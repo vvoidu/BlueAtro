ade813942dc5e46c0d12bf5e7de83108110b2a40
vvoidu <52330651+vvoidu@users.noreply.github.com>
2025-09-01 12:08:57 -0400

    Maid buff

--- src/jokers/041_perfect_maid.lua
+++ src/jokers/041_perfect_maid.lua
@@ -6,13 +6,21 @@ SMODS.Joker({
 	key = "perfect_maid",
 	atlas = "blueatro_joker_atlas",
 	pos = BlueAtro.id_to_atlas_pos(41),
-	config = { extra = {} },
+	config = { extra = { h_size = 2 } },
 	rarity = 1,
 	cost = 4,
 	blueprint_compat = false,
 	eternal_compat = true,
 	perishable_compat = true,
-	loc_vars = function(_, info_queue, card) end,
+	loc_vars = function(_, info_queue, card)
+		return { vars = { card.ability.extra.h_size } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		G.hand:change_size(card.ability.extra.h_size)
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.hand:change_size(-card.ability.extra.h_size)
+	end,
 	calculate = function(self, card, context)
 		if context.before and context.main_eval and not context.blueprint then
 			play_sound("blueatro_e_pyon")
