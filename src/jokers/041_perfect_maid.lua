8481bd4d509c4a24cf1c31720d5221c8d9e9567b
vvoidu <52330651+vvoidu@users.noreply.github.com>
2025-11-22 12:03:44 -0500

    Toki fix
    Forgot to add file

--- src/jokers/041_perfect_maid.lua
+++ src/jokers/041_perfect_maid.lua
@@ -1,3 +1,7 @@
+-- Its effect cannot be implemented with `calculate`
+-- because discarding during scoring doesn't work properly.
+-- See lovely.toki.toml
+
 SMODS.Sound({
 	key = "e_pyon",
 	path = "e_pyon.wav",
@@ -20,17 +24,5 @@ SMODS.Joker({
 	end,
 	remove_from_deck = function(self, card, from_debuff)
 		G.hand:change_size(-card.ability.extra.h_size)
-	end,
-	calculate = function(self, card, context)
-		if context.before and context.main_eval and not context.blueprint then
-			play_sound("blueatro_e_pyon", 1.0, 0.6)
-			for _, c in ipairs(G.hand.cards) do
-				if not c.highlighted then
-					G.hand.highlighted[#G.hand.highlighted + 1] = c
-					c:highlight(true)
-				end
-			end
-			G.FUNCS.discard_cards_from_highlighted({}, true)
-		end
 	end,
 })
